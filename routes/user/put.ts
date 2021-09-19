cd9cac7973223700cad12a1ea72016ddc83f829d
damian <godevdamian@gmail.com>
2021-09-19 16:00:39 +0200

    Added middleware for update avatar route

--- routes/user/put.ts
+++ routes/user/put.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import upload from "../../config/multer";
 import { avatar } from "../../controllers/user/put";
+import verifyToken from "../../middleware/verifyToken";
 import { getPutUserAvatarValidator } from "../../validators/user/put";
 
 const router = Router();
 
-router.put("/avatar", getPutUserAvatarValidator(), avatar);
+router.put("/avatar", getPutUserAvatarValidator(), upload.single("avatar"), verifyToken, avatar);
 
 export default router;
