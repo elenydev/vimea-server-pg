7835cab7492eefe5b5c91ed7298c094415ebae2a
damian <godevdamian@gmail.com>
2021-09-27 19:48:03 +0200

    Routes for get endpoints added to router

--- routes/user/index.ts
+++ routes/user/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import postRoutes from "./post";
 import deleteRoutes from './delete';
 import putRoutes from './put';
+import getRoutes from './get';
 
 const userRoutes = Router();
 
-userRoutes.use("/user", postRoutes);
-userRoutes.use("/user", deleteRoutes);
+userRoutes.use("/user/post", postRoutes);
+userRoutes.use("/user/delete", deleteRoutes);
 userRoutes.use("/user/update/", putRoutes);
+userRoutes.use("/user/get", getRoutes);
 
 export default userRoutes;
